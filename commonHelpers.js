273b18263d4a1a4011adb51e8dc1b986aae41b63
Msjlt <151677911+Msjlt@users.noreply.github.com>
2024-04-05 17:20:03 +0000

    Deploying to gh-pages from @ Msjlt/goit-js-hw-11@35b87b000c5f9217e07145c6b4bf90662c8ec0b5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,25 @@
-import{i as c}from"./assets/vendor-bee2f3af.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const a="43059810-21766dfeafea29ca9c24ae0e2",l=document.getElementById("list"),u=new URLSearchParams({key:a,q:"cat+dog",image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api?${u}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{console.log(r),l.insertAdjacentHTML("beforeend",f(r.hits))}).catch(r=>{c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})});function f(r){return r.map(({id:o,previewURL:s,tags:i})=>`
-        <li data-id="${o}">
-            <img src="${s}" alt="${i}" width="300">
-        </li>
-    `).join("")}
+import{i as l,S as u}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const d="https://pixabay.com/api/",p="43059810-21766dfeafea29ca9c24ae0e2";function f(o=""){const r=new URLSearchParams({key:p,q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${d}?${r}`).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}function m(o){return o.map(({webformatURL:r,largeImageURL:s,tags:n,likes:e,views:t,comments:a,downloads:c})=>`
+        <li class="pictureCard">
+          <a href="${s}" class="lightbox-image">
+            <img src="${r}" alt="${n}" class="picture-icon">
+          </a>
+          <div class="picture-info">
+            <div>
+              <span>Likes:</span>
+              <span class="likes">${e}</span>
+            </div>
+            <div>
+              <span>Views:</span>
+              <span class="views">${t}</span>
+            </div>
+            <div>
+              <span>Comments:</span>
+              <span class="comments">${a}</span>
+            </div>
+            <div>
+              <span>Downloads:</span>
+              <span class="downloads">${c}</span>
+            </div>
+          </div>
+        </li>`).join("")}const i=document.getElementById("search-form"),h=document.getElementById("list");i.addEventListener("submit",y);function y(o){o.preventDefault();const{picture:r}=o.currentTarget.elements;function s(){const e=document.querySelector(".loader");e&&(e.style.display="block")}function n(){const e=document.querySelector(".loader");e&&(e.style.display="none")}s(),f(r.value).then(e=>{e.hits.length===0?l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):(h.innerHTML=m(e.hits),new u(".pictureCard a",{captionType:"attr",captionsData:"alt",captionDelay:250}).refresh())}).catch(e=>{console.error("Error:",e)}).finally(()=>{n(),i.reset()})}
 //# sourceMappingURL=commonHelpers.js.map
